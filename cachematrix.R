c56956998a48c977f8a3e8b5167496e11d3b0515
Ruslan Kharitonov <therk@akumen.net>
2015-01-19 19:40:00 -0500

    Implemented makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A set of funtionct for caching the inverse of a matrix, for it is a costly computation.
 
-## Write a short comment describing this function
+# Need MASS for ginv() function, for solve() function is limited to square numeric or complex matrix only.
+library(MASS)
 
+## The makeCacheMatrix function creates a special "matrix" that caches matrix inversion.
 makeCacheMatrix <- function(x = matrix()) {
-
+  matrixInverse <- NULL
+  set <- function(y) {
+    x <<- y
+    matrixInverse <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) matrixInverse <<- inverse
+  
+  getInverse <- function() matrixInverse
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## The cacheSolve function return a matrix that is the inverse of 'x' from cache 
+## or calculates it if called for the first time on a specific matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  matrixInverse <- x$getInverse()
+  if(!is.null(matrixInverse)) {
+    message("getting cached data")
+    return(matrixInverse)
+  }
+  data <- x$get()
+  # Using ginv() instead of solve() function, because solve() is limited to square matrix and ginv() is not.
+  matrixInverse <- ginv(data, ...)
+  x$setInverse(matrixInverse)
+  matrixInverse
 }
